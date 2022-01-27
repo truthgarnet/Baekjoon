d32deac4bd9603ede806a3a735366a1c9f0bf493
truthgarnet <57488270+truthgarnet@users.noreply.github.com>
2022-01-27 15:55:42 +0900

    Baekjoon18108

---
+++ Baekjoon18108.java
@@ -0,0 +1,15 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Baekjoon18108 {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		System.out.print(n - 543);
+	}
+
+}
